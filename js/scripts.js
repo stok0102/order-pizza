26f21d7e9f01ac605ab06c7f1a3e8abf79bea3ed
Caleb Stokka <caleb.stokka@gmail.com>
2016-06-17 13:06:22 -0700

    changed size property to size object with size and price properties

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,33 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var bigness = parseInt($("input[type='radio'][name='bigness']:checked").val());
+    var sizes = parseInt($("input[type='radio'][name='bigness']:checked").val());
     var toppings = []
-     $.each($("input[name='topping']:checked"), function(){
+    $.each($("input[name='topping']:checked"), function(){
                 toppings.push($(this).val());
             });
-    pizza = new Pizza(bigness, toppings, 0)
+    pizza = new Pizza(toppings, 0)
+    bigness = new Bigness(sizeArray[sizes], sizes)
+    pizza.bignesses.push(bigness);
+    console.log(pizza);
     pizza.cost();
     $("ul").append("<li>This pizza will cost:" + pizza.price + "</li>");
   })
 });
 //back end logic
-function Pizza(bigness, toppings, price) {
-  this.bigness = bigness ;
+function Pizza(toppings, price, bignesses) {
   this.toppings = toppings;
   this.price = price;
+  this.bignesses = [];
 }
 
-Pizza.prototype.cost = function () {
-  this.price = 10 + this.toppings.length + this.bigness;
+function Bigness(sizeValue, sizePrice) {
+  this.sizeValue = sizeValue;
+  this.sizePrice = sizePrice;
+}
+
+var sizeArray = ["small", "medium", "large"]
+
+Pizza.prototype.cost = function (bigness) {
+  this.price = 10 + this.toppings.length + this.bignesses[0].sizePrice;
 }
